3421a6961683f4baad6a58c4a2824f7898e50cc1
niomwungeri-fabrice <niomwungeri.fabrice@gmail.com>
2020-05-08 14:40:10 +0200

    add project structure

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+import startupDebugger from "debug";
+
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/v1", (req, res) =>
+  res.json({ message: "Welcome to taxi 24 Web API v1" })
+);
+app.get("/api/v1/resources", (req, res) => res.json(listEndpoints(app)));
+
+
+const debug = startupDebugger("minutes:start");
+app.listen(port, () => debug(`taxi 24 Web API listening on port ${port}!`));
+
+export default app;
