95ce4dfdbc00ef5e6c3b7d73de3e4357532ef59f
niomwungeri-fabrice <niomwungeri.fabrice@gmail.com>
2020-05-09 12:56:16 +0200

    add get rider by id

--- app/v1/controllers/riderController.js
+++ app/v1/controllers/riderController.js
@@ -1,8 +1,17 @@
 import Rider from "../database/models/Rider";
-
+import constants from "../helpers/constants";
+const { OK, NOT_FOUND } = constants.statusCode;
 export default class RiderControllers {
   static async getAllRiders(req, res) {
     const { rows } = await Rider.getAll();
-    return res.status(200).json(rows);
+    return res.status(OK).json(rows);
+  }
+  static async getRiderById(req, res) {
+    const { id } = req.params;
+    const { rows } = await Rider.findByOne(id);
+    if (!rows[0]) {
+      return res.status(NOT_FOUND).send({ message: "Rider does not exist" });
+    }
+    return res.status(OK).json(rows[0]);
   }
 }
--- app/v1/routes/driverRoutes.js
+++ app/v1/routes/driverRoutes.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import DriverController from "../controllers/driverController";
 import errorHandler from "../middlewares/errorHandler";
-
 const driver = Router();
 
 driver.get("/drivers", errorHandler(DriverController.getAllDrivers));
