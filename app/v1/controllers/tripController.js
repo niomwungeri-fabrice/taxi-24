e2e0b0f0e88617963ef8f4e089a4f5f41cc5c2e6
niomwungeri-fabrice <niomwungeri.fabrice@gmail.com>
2020-05-10 08:54:07 +0200

    add feature to create a trip

--- app/v1/controllers/tripController.js
+++ app/v1/controllers/tripController.js
@@ -1,19 +1,22 @@
 import Trip from "../database/models/Trip";
 import constants from "../helpers/constants";
-const { CREATED } = constants.statusCode;
+import Rider from "../database/models/Rider";
+import Driver from "../database/models/Driver";
+const { CREATED, NOT_FOUND } = constants.statusCode;
 export default class TripController {
   static async createTrip(req, res) {
-    // const {
-    //   departure,
-    //   destination,
-    //   costAmount,
-    //   coordinates,
-    //   riderId,
-    //   driverId,
-    // } = req.body;
-    const { rows } = await Trip.create(req.body);
+    const { riderId, driverId } = req.body;
+    const rider = await Rider.findByOne(riderId);
+    const driver = await Driver.findByOne(driverId);
+    if (!rider.rows[0]) {
+      return res.status(NOT_FOUND).send({ message: "Rider does not exist" });
+    }
+    if (!driver.rows[0]) {
+      return res.status(NOT_FOUND).send({ message: "Driver does not exist" });
+    }
+    const trip = await Trip.create(req.body);
     return res
       .status(CREATED)
-      .json({ message: "Trip created successfully", trip: rows[0] });
+      .json({ message: "Trip created successfully", trip });
   }
 }
